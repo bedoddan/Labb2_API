ee69bb782ae350c8fc49b9b57c6ad98db4e187df
Leo Jonsson <lejo0066@ad.umu.se>
2020-11-25 17:44:43 +0100

    Add project files.

---
+++ Controllers/PrenumerantController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Labb2_API.Modeller;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Labb2_API.Metoder;
+
+namespace Labb2_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PrenumerantController : ControllerBase
+    {
+
+        [HttpGet]
+        public ActionResult<IEnumerable<PrenumerantModell>> GetAllPrenumeranter()
+        {
+            List<PrenumerantModell> prenlista = new List<PrenumerantModell>();
+            PrenumerantMetoder pm = new PrenumerantMetoder();
+            string error = "";
+
+            prenlista = pm.GetPrenumeranter(out error);
+
+            return prenlista;
+        }
+    }
+}
