cf47b0a152b872d53ab962309fd729aaf43a3125
Leo Jonsson <lejo0066@ad.umu.se>
2021-09-30 19:04:45 +0200

    Wille Pille

---
+++ Labb2_MVC/Controllers/AnnonsController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Labb2_MVC.Models;
+using System.Net.Http;
+using Newtonsoft.Json;
+
+namespace Labb2_MVC.Controllers
+{
+    public class AnnonsController : Controller
+    {
+        HelperClass helper = new HelperClass();
+
+        public async Task<IActionResult> Index()
+        {
+            List<PrenumerantModell> Prenumeranter = new List<PrenumerantModell>();
+            HttpClient client = helper.Initial();
+            HttpResponseMessage res = await client.GetAsync("/api/Prenumerant");
+
+            if (res.IsSuccessStatusCode)
+            {
+                var result = res.Content.ReadAsStringAsync().Result;
+                Prenumeranter = JsonConvert.DeserializeObject<List<PrenumerantModell>>(result);
+            }
+
+            return View(Prenumeranter);
+        }
+    }
+}
---
+++ Labb2_MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Labb2_MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
